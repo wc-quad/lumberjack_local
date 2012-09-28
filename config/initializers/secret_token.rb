2af01787cd3541b4000b79e6fc12c21111120500
Gabe Mabry <gsmabry@ncsu.edu>
2012-09-27 21:16:43 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lumberjack::Application.config.secret_token = 'f39ea855e5b9ecec112779f6b606270380ca888675bd33e620db0c50b6e15d086d391c8101e28ad2a6afea8609531116983a11ced40f65738b5eb1a748fafa49'
